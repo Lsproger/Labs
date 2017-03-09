5a6014801d576d1cee50413f7a8f9b712c575d73
Yury <yur4iik@tut.by>
2017-03-09 23:01:15 +0300

    Some changes with .gitignore & .gitattributes

--- 5/CSLaba5/CSLaba5/Program.cs
+++ 5/CSLaba5/CSLaba5/Program.cs
@@ -12,21 +12,21 @@ namespace CSLaba5
     {
         static void Main()
         {
-            // Reflector.ToTXT("CSLaba5.Hunter", "D:\\HunterInfo.txt");
-            // Console.WriteLine("All methods:");
-            // Reflector.ToConsole(Reflector.GetAllMeth("CSLaba5.Hunter"));
-            // Console.WriteLine("\nAll fields & properties:");
-            // Reflector.ToConsole(Reflector.GetFields("CSLaba5.Hunter"));
-            // Console.WriteLine("\nAll interfaces:");
-            // Reflector.ToConsole(Reflector.GetInterf("CSLaba5.Hunter"));
-            // Reflector.GetMethWithParam("CSLaba5.Hunter", "Hunter[]");
+            Reflector.ToTXT("CSLaba5.Hunter", "D:\\HunterInfo.txt");
+            Console.WriteLine("All methods:");
+            Reflector.ToConsole(Reflector.GetAllMeth("CSLaba5.Hunter"));
+            Console.WriteLine("\nAll fields & properties:");
+            Reflector.ToConsole(Reflector.GetFields("CSLaba5.Hunter"));
+            Console.WriteLine("\nAll interfaces:");
+            Reflector.ToConsole(Reflector.GetInterf("CSLaba5.Hunter"));
+            Reflector.GetMethWithParam("CSLaba5.Hunter", "Hunter[]");
 
 
-            //Reflector.CallSomeMeth("CSLaba5.Hunter", "PrintSmth", "D:\\inf.txt");
+            Reflector.CallSomeMeth("CSLaba5.Hunter", "PrintSmth", "D:\\inf.txt");
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
         }
     }
 }
